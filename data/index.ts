974e172d7e1552047f95959cd9f1601539040254
Aeroox <kayroog1@gmail.com>
2024-10-07 13:42:12 +0100

    initial-commit

---
+++ data/index.ts
@@ -0,0 +1,236 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I has prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-2 md:col-span-2 col-span-1 md:row-span-2 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-1 md:col-span-1 col-span-1 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-1 md:col-span-1 col-span-1 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-1 md:col-span-1 col-span-1 md:h-40 ",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a React Native Project",
+      description: "The Inside Scoop",
+      className: "md:col-span-2 col-span-2 col-span-1 md:h-[336px]",
+      imgClassName: "absolute right-0 bottom-0 md:w-96",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "md:-mt-32 lg:col-span-1 col-span-1 md:col-span-1 md:h-40",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 md:-mt-2 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "CarHub",
+      des: "Find, book or rent a car — quickly and easily",
+      img: "/carhub.jpg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/headless-ui.svg"],
+      link: "https://carhub-a.vercel.app",
+    },
+    {
+      id: 2,
+      title: "Briefit - Simplify your reading with summarizer ",
+      des: "Summarize Articles with OPENAI GPT-4 - Simplify your reading with summarizer, an open source summarizer that transforms lenghty articles into clear and conscise summaries",
+      img: "/briefit.jpg",
+      iconLists: ["/re.svg", "/tail.svg", "/javascript.svg", "/vitejs.svg", "redux.svg"],
+      link: "https://briefit.vercel.app",
+    },
+    {
+      id: 3,
+      title: "SuoBank - Digital Bank",
+      des: "Digital banking system for easy and faster monitary banking needs for day to day usage.",
+      img: "/suobank.jpg",
+      iconLists: ["/re.svg", "/tail.svg", "/javascript.svg", "/vitejs.svg"],
+      link: "https://suobank-a.vercel.app",
+    },
+    {
+      id: 4,
+      title: "Aeroox-GPT4 - AI Integrated Open-AI Platform",
+      des: "Build something amazing with Aeroox-GPT-4 Possibilities are beyond your imagination.",
+      img: "/aeroox-gpt4.jpg",
+      iconLists: ["/re.svg", "/css-3.svg", "/javascript.svg"],
+      link: "https://aeroox-4-ai-project.vercel.app",
+    },
+    {
+      id: 5,
+      title: "Members List - React Application",
+      des: "An Exclusive members list for conscise list of our exclusive and dedicated members of our community",
+      img: "/memberlist.jpg",
+      iconLists: ["/next.svg", "/tail.svg", "/javascript.svg",],
+      link: "https://team-list-project.vercel.app",
+    },
+    {
+      id: 6,
+      title: "SuoBank - Digital Bank",
+      des: "Digital banking system for easy and faster monitary banking needs for day to day usage.",
+      img: "/suobank.jpg",
+      iconLists: ["/re.svg", "/tail.svg", "/javascript.svg", "/vitejs.svg"],
+      link: "https://suobank-a.vercel.app",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+    {
+      id: 2,
+      title: "Freelance WebApp Developer",
+      desc: "Lead the dev of a webapp for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2",
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 3,
+      title: "Junior Frontend Developer",
+      desc: "Designed and developed Live Web app using NextJS.",
+      className: "md:col-span-2", 
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 4,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      href: "https://github.com/Aeroox-K"
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      href: "https://x.com/wisdom_kayea"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      href: "https://www.linkedin.com/in/wisdom-kayea-4864b7168/?"
+    },
+  ];
