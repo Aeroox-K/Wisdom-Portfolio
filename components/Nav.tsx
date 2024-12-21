954f3bc54c8c0df2a837d168e69e6ac626da4745
Aeroox <kayroog1@gmail.com>
2024-12-21 21:16:58 +0100

    top-refurb

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image"
+import Link from "next/link";
 
 const Nav = () => {
   return (
     <div className="z-[6001] opacity-90 text-white md:mt-16 mt-10 mr-auto md:ml-16 ml-1 " >
+      <Link href="/" >
       <Image 
         src="Wisdom Logo-W.svg" 
         alt="Wisdom Logo-W.svg"
         width={70}  
         height={70}
       />
+      </Link>
+    
     </div>
   )
 }
