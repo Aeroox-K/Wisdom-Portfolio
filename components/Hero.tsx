8c64e875ac97a9eaa7903afb14f0ab53b1e4697a
Aeroox <kayroog1@gmail.com>
2024-12-26 15:44:42 +0100

    v2-upscale1.1

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,12 +27,12 @@ const Hero = () => {
               {/* Hello, I am */}
             </h2>
             <TextGenerateEffect 
-            className='text-start text-[37px] md:text-5xl lg:text-5xl break-words'
+            className='text-start text-[37px] md:text-5xl lg:text-5xl'
             words='Creative Frontend Developer'
             />
             <p className='text-start justify-center md:w-2/3 w-full md:tracking-wider leading-6 mb-4 text-sm md:text-lg lg:text-lg' >Hi, I&apos;m <span className='text-purple md:border-b-2 border-b border-purple ' >Wisdom Kayea</span> a Frontend Developer passionate about Designing and Developing creative and scalable websites with great performance and user experience.</p>
             
-            <div className='flex items-start gap-4 max-sm:mt-6' >
+            <div className='flex items-start gap-4 max-sm:mt-6 mt-3' >
               <a href="#projects">
                 <MagicButton 
                 title='Show my Work' 
